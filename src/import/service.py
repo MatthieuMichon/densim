bd0be9e8d65a2be9151c9a928a89ae030cd1c5b0
Matthieu Michon <matthieu.michon@gmail.com>
2014-06-03 01:19:07 +0200

    Replaced mission by service

--- src/import/service.py
+++ src/import/service.py
@@ -1,19 +1,19 @@
 import json
 
-class Mission:
-    """Mission class"""
+class Service:
+    """Service class"""
 
     def __init__(self):
         """Default constructor"""
 
-    def __init__(self, mission_json_filename):
+    def __init__(self, service_json_filename):
         """Constructor with JSON filename"""
-        self.json_filename = mission_json_filename
+        self.json_filename = service_json_filename
 
 
-    def set_filename(self, mission_json_filename):
+    def set_filename(self, service_json_filename):
         """Return the JSON filename"""
-        self.json_filename = mission_json_filename
+        self.json_filename = service_json_filename
     
     def get_filename(self):
         """Return the JSON filename"""
