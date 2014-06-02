5628d2ef05c08f7f3c7be6754e3ababca1da9237
Matthieu Michon <matthieu.michon@gmail.com>
2014-06-03 00:48:15 +0200

    Fixed self-test issue.

--- tests/check_tests.py
+++ tests/check_tests.py
@@ -1,8 +1,8 @@
 import check_tests_data
 
 if __name__ == '__main__':
-    json_ok = check_tests_data.check_test_json()
-    if (json_ok == True):
+    json_ok = check_tests_data.check_all_json_per_directory("tests/data")
+    if (json_ok == 0):
         exit(0)
     exit(-1)
 
